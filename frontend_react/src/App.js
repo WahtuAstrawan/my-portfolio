47fd015f51030c8941fc3e8e365499e7fe59119a
Wahtu Astrawan <chatt699@gmail.com>
2023-07-13 20:24:46 +0800

    menginisialisasi proyek dan menambahkan navbar

---
+++ frontend_react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
+
+const App = () => (
+  <div className="app">
+    <Navbar />
+  </div>
+);
+
+export default App;
