0cc731a5026558b8286e81420270df58ec9bc39b
Wahtu Astrawan <chatt699@gmail.com>
2023-07-21 12:56:46 +0800

    menambahkan project section

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -31,7 +31,6 @@ import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
 import asus from '../assets/asus.png';
 import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
 import java from '../assets/java.png';
@@ -67,7 +66,6 @@ export default {
   amazon,
   asus,
   bolt,
-  nb,
   skype,
   spotify,
   java,
