147ad7cf9c000209e8a0a9af3cf2fe39ce7e8427
Wahtu Astrawan <chatt699@gmail.com>
2023-07-26 13:57:09 +0800

    menambahkan skill page dan motion wrap untuk semua section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {motion} from 'framer-motion';
 import {Tooltip as ReactTooltip} from 'react-tooltip';
 
-import {AppWrap} from '../../wrapper';
+import {AppWrap, MotionWrap} from '../../wrapper';
 import {urlFor, client} from '../../client';
 import './Skills.scss';
 
@@ -31,7 +31,7 @@ const Skills = () => {
       <div className='app__skills-container'>
 
         <motion.div className='app__skills-list'>
-          {skills.map((skill) => (
+          {skills?.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0,1] }}
               transition={{ duration:0.5 }}
@@ -46,36 +46,54 @@ const Skills = () => {
           ))}
         </motion.div>
         
-        <motion.div
+        <div
           className='app__skills-exp'
         >
-          {experiences.map((work) => (
-            <>
+          {experiences?.map((experience) => (
+            <motion.div
+              className='app__skills-exp-item'
+              key={experience.year}
+            >
+              <div className='app__skills-exp-year'>
+                <p className='bold-text'>{experience.year}</p>
+              </div>
               <motion.div
-                whileInView={{ opacity: [0,1] }}
-                transition={{ duration:0.5 }}
-                className='app__skills-exp-work'
-                data-tip
-                data-for={work.name}
-                key={work.name}
+                className='app__skills-exp-works'
               >
-                <h4 className='bold-text'>{work.name}</h4>
-                <p className='p-text'>{work.company}</p>
+                {experience?.works?.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0,1] }}
+                      transition={{ duration:0.5 }}
+                      className='app__skills-exp-work'
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className='bold-text'>{work.name}</h4>
+                      <p className='p-text'>{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                ))}
               </motion.div>
-              <ReactTooltip
-                id={work.name}
-                effect='solid'
-                arrowColor='#fff'
-                className='skills-tooltip'
-              >
-                {work.desc}
-              </ReactTooltip>
-            </>
+            </motion.div>
           ))}
-        </motion.div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills;
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'),
+  'skills',
+  "app__whitebg"
+);
